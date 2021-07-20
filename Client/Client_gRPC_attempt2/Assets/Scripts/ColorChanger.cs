b602b188b53cc146d20be3396a4b89a01f3e9196
Levi Yarmak <43060965+lyarmak@users.noreply.github.com>
2021-07-20 12:43:14 -0700

    Small Unity Test Project (server response to print on console)

---
+++ Client/Client_gRPC_attempt2/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Grpc.Core;
+using Protocolor;
+
+public class ColorChanger : MonoBehaviour
+{
+    // Public Fields
+    public Material material;
+    public readonly Color defaultColor = Color.white;
+
+    // Serialized Fields
+    [SerializeField] private ColorGenerator.ColorGeneratorClient _client;
+    [SerializeField] private Channel _channel;
+
+    public void Start()
+    {
+        _channel = new Channel("https://localhost", 5001, ChannelCredentials.Insecure);
+        _client = new ColorGenerator.ColorGeneratorClient(_channel);
+        Debug.Log("Channel and Client established");
+    }
+
+    public void ChangeColor()
+    {
+        material.color = GetColor(material.color);
+        Debug.Log("Color Change Request");
+    }
+
+    public Color GetColor(Color currentColor)
+    {
+        var currentColorSend = ColorUtility.ToHtmlStringRGBA(currentColor);
+        var newColor = GetNewColor(currentColorSend);
+        return Color.red;
+    }
+
+    public string GetNewColor(string currentColor)
+    {
+        _channel = new Channel("https://localhost", 5001, ChannelCredentials.Insecure);
+        _client = new ColorGenerator.ColorGeneratorClient(_channel);
+        var clientRequest = new CurrentColor { Color = currentColor };
+        var result = _client.GetRandomColor(clientRequest);
+        Debug.Log($"Returned Value: { result.Color }");
+        return result.Color;
+    }
+
+    private void OnDisable()
+    {
+        _channel.ShutdownAsync().Wait();
+    }
+}
