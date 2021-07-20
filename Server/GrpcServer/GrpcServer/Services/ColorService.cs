bf4ec0811cd8413ab30dbabdbf0efc7a77785ef9
Levi Yarmak <43060965+lyarmak@users.noreply.github.com>
2021-07-20 12:32:00 -0700

    C# Client Implementation

---
+++ Server/GrpcServer/GrpcServer/Services/ColorService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Protocolor;
+
+namespace GrpcServer.Services
+{
+    public class ColorService : ColorGenerator.ColorGeneratorBase
+    {
+        private readonly ILogger<ColorService> _logger;
+
+        public ColorService(ILogger<ColorService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<NewColor> GetRandomColor(CurrentColor request, ServerCallContext context)
+        {
+            return Task.FromResult(new NewColor
+            {
+                Color = "#0000FF"
+            });
+        }
+    }
+}
